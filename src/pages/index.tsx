b11930df3ca202231bae4d06e91be581c6647d90
Sebas Ramos <sebastian.ramed@gmail.com>
2023-02-15 18:17:22 -0600

    :lipstick: Styled ProductItem component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,35 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-import { Box, Button, CircularProgress, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
-import React from 'react'
-import { useGetProducts } from '@/hooks/useGetProducts'
-import { ProductModel } from '@/types/productTypes'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/home";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
+import React from "react";
+import { useGetProducts } from "@/hooks/useGetProducts";
+import { ProductModel } from "@/types/productTypes";
+import ProductItem from "@/ui/home/components/ProductItem";
 
-export default function Home() {
-  const {isLoading, data} = useGetProducts();
+function Home() {
+  const { isLoading, data } = useGetProducts();
   return (
-    <Box sx={{ 
-      display:'flex',
-      flexDirection: 'column'
-    }}>
-
-    <Box className="filters">
-        <FormControl
-            
-        sx={{ width: ["100%", "300px"], mr: 5}}>
-            <InputLabel id="categories-label">Categories</InputLabel>
-            <Select
-            /*
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Box className="filters">
+        <FormControl sx={{ width: ["100%", "300px"], mr: 5 }}>
+          <InputLabel id="categories-label">Categories</InputLabel>
+          <Select
+          /*
                 IconComponent={() => (
                     <ArrowDropDownIcon sx={{color: selectedTheme.colors.text}} />
                 )}
@@ -34,18 +42,18 @@ export default function Home() {
                     border: `${selectedTheme.name === "Dark" ? `1px solid ${selectedTheme.colors.text}` : '0px'}`, 
                     color: selectedTheme.colors.text}}
                     */
-            >
-                <MenuItem  value={"treasure_hunt"}>Treasure Hunts</MenuItem>
-                <MenuItem  value={"guided_tour"}>Guided Tours</MenuItem>
-                <MenuItem  value={"food_experience"}>Food Experiences</MenuItem>
-                <MenuItem  value={"date_night"}>Date Night</MenuItem>
-                <MenuItem  value={"drink_tour"}>Drink Experiences</MenuItem>
-            </Select>
+          >
+            <MenuItem value={"treasure_hunt"}>Treasure Hunts</MenuItem>
+            <MenuItem value={"guided_tour"}>Guided Tours</MenuItem>
+            <MenuItem value={"food_experience"}>Food Experiences</MenuItem>
+            <MenuItem value={"date_night"}>Date Night</MenuItem>
+            <MenuItem value={"drink_tour"}>Drink Experiences</MenuItem>
+          </Select>
         </FormControl>
-        <FormControl sx={{width: ["100%", "300px"], mr:5, mt:[1, 0]}}>
-            <InputLabel id="sort-label">Sort By</InputLabel>
-            <Select
-            /*
+        <FormControl sx={{ width: ["100%", "300px"], mr: 5, mt: [1, 0] }}>
+          <InputLabel id="sort-label">Sort By</InputLabel>
+          <Select
+          /*
                 IconComponent={() => (
                     <ArrowDropDownIcon sx={{color: selectedTheme.colors.text}} />
                 )}
@@ -58,31 +66,35 @@ export default function Home() {
                     border: `${selectedTheme.name === "Dark" ? `1px solid ${selectedTheme.colors.text}` : '0px'}`, 
                     color: selectedTheme.colors.text}}
                     */
-            >   
-                <MenuItem value="price">Price</MenuItem>
-                <MenuItem  value="duration">Duration</MenuItem>
-                <MenuItem  value="guide">Whoa!Guide</MenuItem>
-                <MenuItem  value="rating">Rating</MenuItem>
-            </Select>
+          >
+            <MenuItem value="price">Price</MenuItem>
+            <MenuItem value="duration">Duration</MenuItem>
+            <MenuItem value="guide">Whoa!Guide</MenuItem>
+            <MenuItem value="rating">Rating</MenuItem>
+          </Select>
         </FormControl>
-        <Button sx={{ mt: [1, 0]}}>Clear filters</Button>
+        <Button sx={{ mt: [1, 0] }}>Clear filters</Button>
+      </Box>
+      {!isLoading ? (
+        <Box sx={styles.experiencesContainer}>
+          {data!.map((product: ProductModel) => {
+            return <ProductItem key={product.id} product={product} />;
+          })}
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            mt: 8,
+            display: "flex",
+            justifyContent: "center",
+            width: "100%",
+          }}
+        >
+          <CircularProgress size="5rem" />
+        </Box>
+      )}
     </Box>
-    {!isLoading ? 
-      <Box className="experiences-container">
-        {data!.map((product: ProductModel, index: number) => {
-
-                return (
-                    <Box key={index} sx={{ gridColumn: 'span 1'}}>
-                        <h1 className="home_title">{product.title}</h1>
-                    </Box>
-                )
-            })}
-        </Box> 
-        : 
-        <Box sx={{ mt: 8, display: 'flex', justifyContent: 'center', width: "100%" }}>
-            <CircularProgress size="5rem"/>
-        </Box>  }
-    
-    </Box>
-  )
+  );
 }
+
+export default Home;
--- src/ui/home/components/ProductItem.tsx
+++ src/ui/home/components/ProductItem.tsx
@@ -1,59 +1,38 @@
-import { Box, Card, CardContent, CardMedia, Link, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, Typography } from "@mui/material";
 import { ProductModel } from "@/types/productTypes";
+import Image from "next/image";
+import styles from "@/styles/productItem";
 
-function ProductItem(props: ProductModel){
-    const {id, title, price, category, image, description} = props;
-    return(
-        <Card sx={{
-            borderRadius:3,
-            boxShadow:8,
-            width: '100%'
-            }}>
-        <CardMedia
-            className={"experience-image"}
-            component="img"
-            image={image}/>
-                <CardContent
-                sx={{height: 200, color: "#2d2d2d"}}
-                >
-                    <div className="card-container">
-                        <Box>
-                            <Typography sx={{fontWeight: 'bold', fontSize: 22, lineHeight: "22px", fontFamily: "Montserrat"}}>
-                                {props.title}
-                            </Typography>
-                            <Box sx={{ 
-                                fontStyle: 'italic',
-                                fontWeight: 'light',
-                                fontSize: '16px',
-                                mb: 1,
-                                mt: 1,
-                                color:'gray'
-                                }}>
-                                {title}
-                            </Box>
-                        </Box>
-                        
-                        <Box sx={{ 
-                            fontWeight: 'light',
-                            fontSize: '14px',
-                            color: "#525252",
-                            mb: 1
-                            }}>
-                                <b>Category: </b>{category}
-                        </Box>
-                        <Box sx={{height: '75px', overflow: 'hidden'}}>
-                            
-                                {description}
-                            
-                        </Box>
-                    </div>
-                    
-                </CardContent>
-                <Box sx={{display: 'grid', gridTemplateRows: '25px'}}>             
-                    
-                </Box>
-        </Card>
-    ); 
+function ProductItem({ product }: { product: ProductModel }) {
+  const { id, title, price, category, image, description } = product;
+  return (
+    <Box sx={styles.container}>
+      <Card sx={styles.cardContainer}>
+        <CardContent sx={styles.cardContent}>
+          <Box sx={styles.imageContainer}>
+            <Image src={image} alt="" fill />
+          </Box>
+
+          <Box sx={styles.textContainer}>
+            <Typography sx={styles.title}>{title}</Typography>
+            <Box sx={styles.priceContainer}>
+              <Typography>US$</Typography>
+              <Typography sx={styles.price}>{price}</Typography>
+            </Box>
+          </Box>
+          <Box sx={styles.buttonContainer}>
+            <Button
+              onClick={() => console.log("pressed")}
+              variant="contained"
+              sx={styles.button}
+            >
+              Add to cart
+            </Button>
+          </Box>
+        </CardContent>
+      </Card>
+    </Box>
+  );
 }
 
 export default ProductItem;
