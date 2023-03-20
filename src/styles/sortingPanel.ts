edaaf2cd8ec1b116dd7152a07e7d9733e8e2c925
Sebas Ramos <sebastian.ramed@gmail.com>
2023-03-20 17:26:48 -0600

    :lipstick: Styled Sorting Panel

--- src/styles/sortingPanel.ts
+++ src/styles/sortingPanel.ts
@@ -3,14 +3,15 @@ import { SxProps } from "@mui/material";
 const container: SxProps = {
   display: "flex",
   flexDirection: ["column", "row"],
-  mt: 2,
+  mt: 3,
   justifyContent: ["center", "flex-start"],
   alignItems: "center",
   width: "100%",
+  height: [100, 60],
 };
 
 const formControl: SxProps = {
-  width: ["100%", "300px"],
+  width: ["100%", "25%"],
   mr: [0, 5],
   mt: [1, 0],
 };
@@ -18,11 +19,18 @@ const formControl: SxProps = {
 const selectInput: SxProps = {
   border: "1px solid #525252",
   color: "#525252",
-  width: "100%",
+  height: "100%",
 };
 
 const clearFiltersButton: SxProps = {
   mt: [1, 0],
+  color: "#525252",
+  fontWeight: "bold",
+  height: "100%",
+  width: ["100%", "10%"],
+  "&:hover": {
+    backgroundColor: "lightgray",
+  },
 };
 
 const styles = { container, formControl, selectInput, clearFiltersButton };
